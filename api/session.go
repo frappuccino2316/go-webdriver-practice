3498c36f6b57cb0b05167cda08c509d71a52f145
frappuccino2316 <frappuccino2316@gmail.com>
2023-01-07 00:52:45 +0900

    sessionへのPOSTを切り出した

---
+++ api/session.go
@@ -0,0 +1,56 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+type sessionResponse struct {
+	Value struct {
+		SessionID    string      `json:"sessionId"`
+		Capabilities interface{} `json:"capabilities"`
+	} `json:"value"`
+}
+
+func NewSession(client *http.Client) (string, error) {
+	startUrl := "http://localhost:9515/session"
+
+	cap := `
+{
+	"capabilities": {
+		"alwaysMatch": {
+			"goog:chromeOptions": {
+				"args": ["--no-sandbox"]
+			}
+		}
+	}
+}
+`
+
+	req, err := http.NewRequest("POST", startUrl, strings.NewReader(cap))
+
+	if err != nil {
+		return "", err
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Printf("ReadAll Error! %v\n", err)
+		return "", err
+	}
+
+	var data sessionResponse
+	if err := json.Unmarshal(body, &data); err != nil {
+		return "", err
+	}
+
+	return data.Value.SessionID, nil
+}
